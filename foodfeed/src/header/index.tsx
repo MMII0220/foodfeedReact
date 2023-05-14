e03c66df1a79db3d54ba1ed6e8ac9b4181209984
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2023-05-14 08:07:49 +0300

    init: start project

---
+++ foodfeed/src/header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='header__logo'>FoodFeed</div>
+    </header>
+  );
+};
+
+export default Header;
